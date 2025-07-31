5b1cad95589b9e67aedb4bbaa7ad33f48bf8ff94
VanessaGardingo <128542049+VanessaGardingo@users.noreply.github.com>
2025-07-31 19:52:28 -0300

    Add files via upload

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('v1', function ($routes) {
+     $routes->resource('posts', ['controller' => 'PostController', 'except' => ['new', 'edit']]);
+});
